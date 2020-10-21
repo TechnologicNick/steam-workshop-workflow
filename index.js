459dc148cb9abbaecd0b8436d09d3e57df816b8e
TechnologicNick <technologicnick@gmail.com>
2020-10-21 04:15:25 +0200

    Can now update readme files
    Also added a blank content image

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ class ItemDisplay {
 
     generateImages() {
         this.generatePreview("preview.png", 200, 200);
+        this.generateContent("content.png", 600, 200);
     }
 
     async generatePreview(filename, width, height) {
@@ -45,7 +46,29 @@ class ItemDisplay {
 
         context.drawImage(preview, 0, height/2 - (200 / aspectRatio)/2, width, 200 / aspectRatio);
 
-        await new Promise((resolve, reject) => {
+        await this.saveFile(canvas, filename);
+
+        console.log(`[${this.details.publishedfileid}] Generated preview.png`);
+    }
+
+    async generateContent(filename, width, height) {
+        const canvas = createCanvas(width, height);
+        const context = canvas.getContext("2d");
+
+        // let preview = await loadImage(this.details.preview_url);
+        // let aspectRatio = preview.width/preview.height;
+
+        // context.drawImage(preview, 0, height/2 - (200 / aspectRatio)/2, width, 200 / aspectRatio);
+        context.fillStyle = "rgba(0, 0, 0, 0.5)";
+        context.fillRect(0, 0, width, height);
+
+        await this.saveFile(canvas, filename);
+
+        console.log(`[${this.details.publishedfileid}] Generated content.png`);
+    }
+
+    async saveFile(canvas, filename) {
+        return new Promise((resolve, reject) => {
             const out = fs.createWriteStream(this.path + filename);
             const stream = canvas.createPNGStream();
             stream.pipe(out)
@@ -53,10 +76,31 @@ class ItemDisplay {
             out.on("finish", resolve);
             out.on("error", reject);
         });
+    }
+}
 
-        console.log(`[${this.details.publishedfileid}] Generated preview`);
+class WorkshopShowcase {
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    writeShowcase(itemDisplays, commentTag = "WORKSHOP-SHOWCASE") {
+        let content = fs.readFileSync(this.filename, {encoding:"utf8", flag:"r"});
+        console.log("IN:", content);
+
+        let startTag = `<!-- ${commentTag}:START -->`;
+        let endTag = `<!-- ${commentTag}:END -->`
+
+        let middleText = "sample text";
+        let startText = content.substring(0, content.indexOf(startTag) + startTag.length);
+        let endText = content.substring(content.indexOf(endTag));
+
+        let out = startText + "\n" + middleText + "\n" + endText;
+        console.log("OUT:", out);
+        fs.writeFileSync(this.filename, out);
     }
 }
 
 module.exports.SteamWorkshop = SteamWorkshop;
 module.exports.ItemDisplay = ItemDisplay;
+module.exports.WorkshopShowcase = WorkshopShowcase;
--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const { SteamWorkshop, ItemDisplay } = require(".");
+const { SteamWorkshop, ItemDisplay, WorkshopShowcase } = require(".");
+const fs = require('fs');
+
+const template = `# Example README
+My Steam Workshop items:
+<!-- WORKSHOP-SHOWCASE:START -->
+<!-- WORKSHOP-SHOWCASE:END -->
+
+# Another header
+And even more text!
+`;
 
 (async ()=>{
     const workshop = new SteamWorkshop(process.env.STEAM_API_KEY);
@@ -6,10 +16,17 @@ const { SteamWorkshop, ItemDisplay } = require(".");
     let details = await workshop.getDetails(881254777);
     console.log(details);
 
+    let itemDisplays = {}
+
     for (let i = 0; i < details.length; i++) {
         const item = details[i];
         
         let display = new ItemDisplay(item, `./test/workshop/${item.publishedfileid}/`);
         display.generateImages();
     }
+
+    fs.writeFileSync("./test/README.md", template);
+    let showcase = new WorkshopShowcase("./test/README.md");
+    showcase.writeShowcase(itemDisplays);
+
 })();
