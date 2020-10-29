c41d72cb024bc0b58258c05ab995666b02fd2008
TechnologicNick <technologicnick@gmail.com>
2020-10-29 21:12:37 +0100

    Turns out GitHub blocks image loading from SVG, reverting
    https://stackoverflow.com/a/21521184/12586927

--- index.js
+++ index.js
@@ -62,8 +62,7 @@ class ItemDisplay {
             preview_width: preview_width,
             details: this.details,
             imagePath: this.imagePath,
-            info: this.info,
-            github_raw_path: core.getInput("github_raw_path", {required: true})
+            info: this.info
         });
 
         fs.writeFileSync(path.join(this.imagePath, filename), data);
